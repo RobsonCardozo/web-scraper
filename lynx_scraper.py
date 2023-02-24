25a2efa2ca1317e4b1f7aa8c65ea4ec6ea858fbe
robsoncardozo <robsonleaocardozo@hotmail.com>
2023-02-24 15:10:32 -0300

    tkinter implemented

--- lynx_scraper.py
+++ lynx_scraper.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import json
 import random
+import tkinter as tk
 
 def extract_data_to_json():
     api_endpoint = "https://en.wikipedia.org/w/api.php"
@@ -42,5 +43,32 @@ def extract_data_to_json():
             print(response.content)
     else:
         print(f"Failed to retrieve data from the Wikipedia API. Status code: {response.status_code}")
+        
+    # Create a Tkinter window to display the data
+    window = tk.Tk()
+    window.title("Wikipedia Categories")
+    window.geometry("600x400")
+    
+    # Create labels to display the data
+    title_label = tk.Label(window, text=f"Title: {data['title']}", font=("Arial", 14))
+    title_label.pack(pady=10)
+    
+    description_label = tk.Label(window, text=f"Description: {data['description']}", font=("Arial", 12), wraplength=500, justify="left")
+    description_label.pack(pady=10)
+    
+    categories_label = tk.Label(window, text=f"Categories: {', '.join(data['categories'])}", font=("Arial", 12), wraplength=500, justify="left")
+    categories_label.pack(pady=10)
+    
+    images_label = tk.Label(window, text=f"Images: {', '.join(data['images'])}", font=("Arial", 12), wraplength=500, justify="left")
+    images_label.pack(pady=10)
+    
+    external_links_label = tk.Label(window, text=f"External Links: {', '.join(data['external_links'])}", font=("Arial", 12), wraplength=500, justify="left")
+    external_links_label.pack(pady=10)
+    
+    # Create a button to close the window
+    close_button = tk.Button(window, text="Close", command=window.destroy)
+    close_button.pack(pady=10)
+    
+    window.mainloop()
 
 extract_data_to_json()
--- main.py
+++ main.py
@@ -1,14 +1,36 @@
 import json
-from extract_wikipedia import extract_data_to_json
+import tkinter as tk
 
-def main():
-    extract_data_to_json()
+def display_categories():
     with open("data.json") as f:
         data = json.load(f)
-        if data["categories"] is None:
-            print("No categories found on the page.")
-        else:
-            print(data["categories"])
+    
+    # Create a Tkinter window to display the categories
+    window = tk.Tk()
+    window.title("Wikipedia Categories")
+    window.geometry("800x600")
+    
+    # Create labels to display the data
+    title_label = tk.Label(window, text=f"Title: {data['title']}", font=("Arial", 14))
+    title_label.pack(pady=10)
+    
+    description_label = tk.Label(window, text=f"Description: {data['description']}", font=("Arial", 12), wraplength=750, justify="left")
+    description_label.pack(pady=10)
+    
+    categories_label = tk.Label(window, text=f"Categories: {', '.join(data['categories'])}", font=("Arial", 12), wraplength=750, justify="left")
+    categories_label.pack(pady=10)
+    
+    images_label = tk.Label(window, text=f"Images: {', '.join(data['images'])}", font=("Arial", 12), wraplength=750, justify="left")
+    images_label.pack(pady=10)
+    
+    external_links_label = tk.Label(window, text=f"External Links: {', '.join(data['external_links'])}", font=("Arial", 12), wraplength=750, justify="left")
+    external_links_label.pack(pady=10)
+
+    # Create a button to close the window
+    close_button = tk.Button(window, text="Close", font=("Arial", 12), command=window.destroy)
+    close_button.pack(pady=20)
+
+    window.mainloop()
 
 if __name__ == "__main__":
-    main()
+    display_categories()
