327919cb3f837ebc56c75fb41076804fb5a44230
robsoncardozo <robsonleaocardozo@hotmail.com>
2023-02-24 14:19:35 -0300

    API etiquette updated

--- extract_wikipedia.py
+++ extract_wikipedia.py
@@ -1,40 +1,45 @@
 import requests
-from bs4 import BeautifulSoup
 import json
 
 
 def extract_data_to_json():
-    api_endpoint = "https://en.wikipedia.org/w/api.php"
+    api_endpoint = "https://www.mediawiki.org/w/api.php"
     params = {
-        "action": "parse",
+        "action": "query",
         "format": "json",
-        "prop": "text|categories|references"
+        "titles": "MediaWiki",
+        "prop": "categories|images|extlinks",
+        "cllimit": "max",
+        "imlimit": "max",
+        "ellimit": "max",
     }
     response = requests.get(api_endpoint, params=params)
-    data = {"title": "", "categories": [], "references": []}
+    data = {"title": "", "categories": [], "images": [], "external_links": []}
     if response.ok:
         try:
-            page_data = response.json()["parse"]["text"]["*"]
-            soup = BeautifulSoup(page_data, "html.parser")
-            categories_list = soup.find_all("div", {"class": "catlinks"})
-            if categories_list:
-                categories_list = categories_list[0].find_all("li")
-                for category in categories_list:
-                    data["categories"].append(category.text)
-            else:
-                print("No categories found on the page.")
-            references_list = soup.find_all("ol", {"class": "references"})
-            if references_list:
-                for reference in references_list:
-                    data["references"].append(reference.text)
-            else:
-                print("No references found on the page.")
-            data["title"] = response.json()["parse"]["title"]
+            page_data = response.json()["query"]["pages"]
+            for _, page in page_data.items():
+                data["categories"] += [
+                    category["title"] for category in page.get("categories", [])
+                ]
+                data["images"] += [image["title"] for image in page.get("images", [])]
+                data["external_links"] += [
+                    link["*"] for link in page.get("extlinks", [])
+                ]
+                data["title"] = page.get("title", "")
             with open("data.json", "w") as f:
                 json.dump(data, f, indent=4)
         except KeyError:
-            print("Failed to parse data from the Wikipedia API response.")
+            print("Failed to parse data from the MediaWiki API response.")
             print("Response content:")
             print(response.content)
     else:
-        print(f"Failed to retrieve data from the Wikipedia API. Status code: {response.status_code}")
+        print(
+            f"Failed to retrieve data from the MediaWiki API. Status code: {response.status_code}"
+        )
+    return data
+
+
+if __name__ == "__main__":
+    data = extract_data_to_json()
+    print(data["categories"])
